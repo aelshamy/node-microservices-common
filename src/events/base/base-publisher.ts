d19e6fdda99344fe004a13b6c6d860a9bc0af039
Amr Elshamy <ammohamed@icct.com>
2020-09-10 13:56:48 -0400

    Updates

---
+++ src/events/base/base-publisher.ts
@@ -0,0 +1,20 @@
+import { Stan } from 'node-nats-streaming';
+import { BaseEvent } from './base-event';
+
+export abstract class BasePublisher<T extends BaseEvent> {
+  abstract subject: T['subject'];
+
+  constructor(private client: Stan) {}
+
+  publish(data: T['data']): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (error) => {
+        if (error) {
+          return reject(error);
+        }
+        console.log('Event Published to subject ', this.subject);
+        resolve();
+      });
+    });
+  }
+}
